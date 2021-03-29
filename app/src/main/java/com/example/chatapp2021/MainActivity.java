5dc67813fcc16c950dd20838624a4ba31293dad2
DONG <lizill@naver.com>
2021-03-29 21:19:44 +0900

    practice android

--- app/src/main/java/com/example/chatapp2021/MainActivity.java
+++ app/src/main/java/com/example/chatapp2021/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     EditText etId, etPassword;
     ProgressBar progressBar;
+    String userID, userPW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,52 +37,96 @@ public class MainActivity extends AppCompatActivity {
         etPassword = (EditText) findViewById(R.id.etPassword);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-        Button btnLogin = (Button) findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String userID = etId.getText().toString();
-                String userPW = etPassword.getText().toString();
+        SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
 
-                if(userID.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please insert Email", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if(userPW.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please insert Password", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                progressBar.setVisibility(View.VISIBLE);
+        // getString의 첫 번째 인자는 저장될 키, 두 번쨰 인자는 값입니다.
+        // 첨엔 값이 없으므로 키값은 원하는 것으로 하시고 값을 null을 줍니다.
+        String loginId = auto.getString("inputId",null);
+        String loginPwd = auto.getString("inputPwd",null);
 
-                Response.Listener<String> responseLister = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if(success) {
-                                Intent intent = new Intent(MainActivity.this, GroupActivity.class);
-                                MainActivity.this.startActivity(intent);
-                                finish();
-                            }
-                            else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                                builder.setMessage("계정을 다시 확인하세요.")
-                                        .setNegativeButton("다시 시도", null)
-                                        .create()
-                                        .show();
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
+        if(loginId !=null && loginPwd != null) {
+
+            Response.Listener<String> responseLister = new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        boolean success = jsonResponse.getBoolean("success");
+                        if (success) {
+                            Toast.makeText(MainActivity.this, loginId +"님 자동로그인 입니다.", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                            builder.setMessage("계정을 다시 확인하세요.")
+                                    .setNegativeButton("다시 시도", null)
+                                    .create()
+                                    .show();
                         }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                };
-                LoginRequest loginRequest = new LoginRequest(userID, userPW, responseLister);
-                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-                queue.add(loginRequest);
+                }
+            };
+            LoginRequest loginRequest = new LoginRequest(loginId, loginPwd, responseLister);
+            RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+            queue.add(loginRequest);
+        }
+        //id와 pwd가 null이면 Mainactivity가 보여짐.
+        else if(loginId == null && loginPwd == null) {
+
+            Button btnLogin = (Button) findViewById(R.id.btnLogin);
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    userID = etId.getText().toString();
+                    userPW = etPassword.getText().toString();
+
+                    if (userID.isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Please insert Email", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    if (userPW.isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Please insert Password", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    Response.Listener<String> responseLister = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean success = jsonResponse.getBoolean("success");
+                                if (success) {
+                                    //auto의 loginId와 loginPwd에 값을 저장
+                                    SharedPreferences.Editor autoLogin = auto.edit();
+                                    autoLogin.putString("inputId", userID);
+                                    autoLogin.putString("inputPwd", userPW);
+                                    autoLogin.commit();
+                                    Intent intent = new Intent(MainActivity.this, GroupActivity.class);
+                                    MainActivity.this.startActivity(intent);
+                                    finish();
+                                } else {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                    builder.setMessage("계정을 다시 확인하세요.")
+                                            .setNegativeButton("다시 시도", null)
+                                            .create()
+                                            .show();
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+                    LoginRequest loginRequest = new LoginRequest(userID, userPW, responseLister);
+                    RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                    queue.add(loginRequest);
 //                Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_LONG).show();
-            }
-        });
+                }
+            });
+        }
 
         Button btnRegister = (Button) findViewById(R.id.btnRegister);
         btnRegister.setOnClickListener(new View.OnClickListener() {
